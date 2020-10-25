b24631c3027813951cec4975713987495144cbf5
Maicius <maicius@outlook.com>
2020-10-25 12:42:35 +0800

    添加点注释

--- spider.py
+++ spider.py
@@ -2,7 +2,6 @@ import requests
 import pdfkit
 import re
 import datetime
-from bs4 import BeautifulSoup
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import os
 
@@ -15,6 +14,7 @@ class banyuetan():
         "host": "mp.weixin.qq.com"}
 
     count = 1000
+    # 默认是10月21日的时政小测验
     url = 'https://mp.weixin.qq.com/s/i9JfkYZUY2WD3eRz2Md8vg'
     pre_path = './pdf/'
     # 停止时间
@@ -30,13 +30,10 @@ class banyuetan():
             res = self.req.get(self.url,headers=self.headers).text
             next_href_reg = re.compile('(http://mp.weixin.qq.com/s\?[_a-zA-Z0-9\;=&]+)\#wechat_redirect\"')
             text_val_reg = re.compile('textvalue=\"([0-9]+)\"')
+            # 上一天测验的链接，如果获取不到，就结束
             next_href = re.findall(next_href_reg, res)[-1]
             text_val = re.findall(text_val_reg, res)[0]
             dd = datetime.datetime.strptime(text_val, "%Y%m%d") + datetime.timedelta(days = 1)
-            soup = BeautifulSoup(res, "html5lib")
-            # answer_list = soup.find_all(styles=self.answers_style)
-            # answer_list.clear()
-            # res = soup.section.text
             dd_timestamp = dd.replace(tzinfo=datetime.timezone.utc).timestamp()
 
             if dd_timestamp < self.last_year_stamp:
@@ -45,6 +42,8 @@ class banyuetan():
             print("正在转换date...", dc)
             file_path = self.pre_path + dc + '.pdf'
             try:
+                # 这一步比较花时间，可以异步操作
+                # 但这样的好处是可以避免爬虫太过频繁触发反爬
                 pdfkit.from_string(res, file_path, configuration=self.confg)
             except BaseException as e:
                 pass
@@ -52,7 +51,7 @@ class banyuetan():
                 # 删掉每个pdf的最后一页
                 pdf = PdfFileReader(file_path)
                 pdf_writer = PdfFileWriter()
-                for page in range(pdf.getNumPages() - 1):
+                for page in range(pdf.getNumPages()):
                     pdf_writer.addPage(pdf.getPage(page))
 
                 with open(file_path, 'wb') as output_pdf:
@@ -61,7 +60,11 @@ class banyuetan():
             self.url = next_href
             self.index += 1
     def run(self):
-        self.create_pdf()
+        try:
+            self.create_pdf()
+        except BaseException as e:
+            print(e)
+            print("爬虫结束，开始合并pdf...")
         self.combine_all_pdf()
 
     def combine_all_pdf(self):
@@ -70,8 +73,7 @@ class banyuetan():
         pdf_writer = PdfFileWriter()
         for file in files:
             pdf = PdfFileReader(os.path.join(self.pre_path, file))
-
-            for page in range(pdf.getNumPages()):
+            for page in range(pdf.getNumPages() - 1):
                 pdf_writer.addPage(pdf.getPage(page))
 
         with open("半月谈时政小测验.pdf", 'wb') as output_pdf:
